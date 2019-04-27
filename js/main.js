efe84192103517529fd5692da889ad65de43e6e9
MuttakinHasib <hasibmolla28@gmail.com>
2019-04-27 14:23:33 +0600

    First Upload

---
+++ js/main.js
@@ -0,0 +1,30 @@
+TweenMax.from(".form-logo", 1.8, {
+    opacity: 0,
+    delay: 7.8,
+    ease: Expo.easeInOut,
+    y: 30
+});
+TweenMax.from(".form-title", 1.8, {
+    opacity: 0,
+    delay: 8.2,
+    ease: Expo.easeInOut,
+    y: 30
+});
+TweenMax.from("#username", 1.8, {
+    opacity: 0,
+    delay: 8.4,
+    ease: Expo.easeInOut,
+    y: 30
+});
+TweenMax.from("#password", 1.8, {
+    opacity: 0,
+    delay: 8.6,
+    ease: Expo.easeInOut,
+    y: 30
+});
+TweenMax.from("#btn", 1.8, {
+    opacity: 0,
+    delay: 8.8,
+    ease: Expo.easeInOut,
+    y: 30
+});
